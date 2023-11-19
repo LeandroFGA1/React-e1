1a9a7d113271733a52cd31c73ebe647c584eb0ae
LeandroFGA1 <aucache.leandro@gmail.com>
2023-11-19 18:43:57 -0300

    about

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import Header from './components/header/Header.jsx'
 import Hero from './components/hero/Hero.jsx'
 import Store from './components/store/Store.jsx'
+import About from './components/about/About.jsx'
 
 ReactDOM.createRoot(document.getElementById("header")).render(
   <React.StrictMode>
@@ -15,5 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Hero/>
     <Store/>
+    <About/>
   </React.StrictMode>,
 )
