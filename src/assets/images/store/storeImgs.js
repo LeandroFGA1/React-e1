1a938531a89b66237078f762cb54b560f8299510
LeandroFGA1 <aucache.leandro@gmail.com>
2023-12-14 19:33:32 -0300

    imgs store

--- src/assets/images/store/storeImgs.js
+++ src/assets/images/store/storeImgs.js
@@ -4,6 +4,15 @@ import dota2 from "./dota-2.jpg"
 import fifa2010 from "./fifa-2010.webp"
 import genshinImpact from "./genshin-impact.jpg"
 import metalGearSolid3 from "./metal-gear-solid-3.jpg"
+import theLegendOfZelda from "./the-Legend-Of-Zelda.webp"
+import rocketLeague from "./Rocket-League.jpg"
+import apexLegends from "./Apex-Legends.jpg"
+import fortnite from "./Fortnite.jpg"
+import minecraft from "./Minecraft.webp"
+import redDeadRedemption2 from "./Red-Dead-Redemption-2.jpg"
+import overwatch from "./Overwatch.webp"
+import fallout4 from "./Fallout-4.webp"
+import assassinsCreedOdyssey from "./Assassins-Creed-Odyssey.webp"
 // import a from "./"
 export default{
     "cardDefault":cardDefault,
@@ -11,5 +20,15 @@ export default{
     "dota2":dota2,
     "fifa2010":fifa2010,
     "genshinImpact":genshinImpact,
-    "metalGearSolid3":metalGearSolid3
+    "metalGearSolid3":metalGearSolid3,
+    "theLegendOfZelda": theLegendOfZelda,
+    "rocketLeague":rocketLeague,
+    "apexLegends":apexLegends,
+    "fortnite":fortnite,
+    "minecraft":minecraft,
+    "redDeadRedemption2":redDeadRedemption2,
+    "overwatch":overwatch,
+    "fallout4":fallout4,
+    "assassinsCreedOdyssey":assassinsCreedOdyssey
+
 }
