99f19514f06440db128478fefaa584b37482f25b
LeandroFGA1 <aucache.leandro@gmail.com>
2024-01-22 22:27:25 -0300

    pre-suport-form

--- src/components/form/SuportForm.jsx
+++ src/components/form/SuportForm.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
+
+const FormSuportContainer = styled.form`
+    width: 90%;
+    height: fit-content;
+    margin-left: 5%;
+    margin-bottom: 100px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    background-color: var(--white);
+    border-radius: 5px;
+    label {
+        margin: 20px 0px 20px 30px;
+        input,textarea{
+            margin-left: 5px;
+            border: none;
+            background-color: var(--white);
+            border-bottom: 2px solid grey;
+            min-width: 100px;
+            &:focus{
+                outline: none;
+                
+            }
+            &:active{
+            
+            }
+        }
+    }
+`;
+
+const NameContainer = styled.label``;
+const CategoryContainer = styled.label``;
+const DescriptionContainer = styled.label``;
+const EmailContainer = styled.label``;
 
 const SupportForm = () => {
     const formik = useFormik({
@@ -22,62 +57,63 @@ const SupportForm = () => {
     });
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <label>
-                Name:
-                <input
-                    type="text"
-                    name="name"
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    required
-                />
-                {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
-            </label>
+        <>
+            <FormSuportContainer onSubmit={(e) => { e.preventDefault(); formik.handleSubmit(e); }}>
+                <NameContainer>
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                    />
+                    {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+                </NameContainer>
+                <CategoryContainer>
+                    Category:
+                    <select name="category" value={formik.values.category} onChange={formik.handleChange} onBlur={formik.handleBlur} required>
+                        <option value="">Select a category</option>
+                        <option value="bug">Report a bug</option>
+                        <option value="payment_issue">Payment issues</option>
+                        <option value="suggestion">Suggestion</option>
+                        <option value="others">Others</option>
+                        {/* Agrega aquí más opciones según sea necesario */}
+                    </select>
+                    {formik.touched.category && formik.errors.category ? <div>{formik.errors.category}</div> : null}
+                </CategoryContainer>
 
-            <label>
-                Category:
-                <select name="category" value={formik.values.category} onChange={formik.handleChange} onBlur={formik.handleBlur} required>
-                    <option value="">Select a category</option>
-                    <option value="bug">Report a bug</option>
-                    <option value="payment_issue">Payment issues</option>
-                    <option value="suggestion">Suggestion</option>
-                    <option value="others">Others</option>
-                    {/* Aqui se agrega */}
-                </select>
-                {formik.touched.category && formik.errors.category ? <div>{formik.errors.category}</div> : null}
-            </label>
+                <DescriptionContainer>
+                    Description:
+                    <textarea
+                        name="description"
+                        value={formik.values.description}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                    />
+                    {formik.touched.description && formik.errors.description ? <div>{formik.errors.description}</div> : null}
+                </DescriptionContainer>
 
-            <label>
-                Description:
-                <textarea
-                    name="description"
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    required
-                />
-                {formik.touched.description && formik.errors.description ? <div>{formik.errors.description}</div> : null}
-            </label>
+                <EmailContainer>
+                    Email:
+                    <input
+                        type="email"
+                        name="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        required
+                    />
+                    {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                </EmailContainer>
 
-            <label>
-                Email:
-                <input
-                    type="email"
-                    name="email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    required
-                />
-                {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
-            </label>
+                {/* Agrega aquí más campos según sea necesario para tu formulario de soporte */}
 
-            {/* Agrega aqui si se necesita */}
-
-            <button type="submit">Submit</button>
-        </form>
+                <button type="submit">Submit</button>
+            </FormSuportContainer>
+        </>
     );
 };
 
--- src/pages/suport/Suport.jsx
+++ src/pages/suport/Suport.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import SupportForm from '../../components/form/SuportForm'
+import styled from 'styled-components'
+
+const IntroductionContainer = styled.div`
+    width: 100%;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    p{
+        width: 90%;
+    }
+`;
 
 const Suport = () => {
     return (
-        <SupportForm/>
+        <>
+            <IntroductionContainer>
+                <p>
+                    Welcome to Game Shop, where we deeply value the direct engagement with our dedicated community of gamers. Your insights, whether they involve bug reports, questions, or valuable suggestions, play a crucial role in enhancing the overall gaming experience.
+                </p>
+                <p>
+                    Our commitment is to be readily available to address your needs. We encourage you to utilize the form below to connect with our support team. Whether you've encountered a technical glitch, have inquiries about payments, or want to share your creative suggestions, your input is invaluable to us.
+                </p>
+                <p>
+                    At Game Shop, fostering a vibrant and interactive gaming community is at the heart of what we do. Your feedback is instrumental in shaping the future of our platform. Thank you for being a part of our gaming family, and we look forward to hearing from you!
+                </p>
+            </IntroductionContainer>
+            
+            <SupportForm/>
+        </>
+        
     )
 }
 
